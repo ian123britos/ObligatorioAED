c2057e52307c7dd9ff9cdb1d288b8499dd7aa81f
sebaegonzalez <sebagonzalez987l@gmail.com>
2026-06-03 18:14:44 -0300

    Ejercicio 6

--- SistemaReservasDeVuelo/src/SistemaAutogestion/ISistema.java
+++ SistemaReservasDeVuelo/src/SistemaAutogestion/ISistema.java
@@ -8,5 +8,6 @@ public interface ISistema {
     Retorno registrarPasajero(String cedula, String nombre, int edad,Categoria categoria);
     Retorno buscarPasajero(String cedula);
     Retorno listarPasajerosAscendente();
+    Retorno listarPasajerosDescendente();
 
 }
--- SistemaReservasDeVuelo/src/SistemaAutogestion/ImplementacionSistema.java
+++ SistemaReservasDeVuelo/src/SistemaAutogestion/ImplementacionSistema.java
@@ -29,82 +29,72 @@ public class ImplementacionSistema implements ISistema {
 
         return new Retorno(Retorno.Resultado.OK);
     }
-    
+
     @Override
-    public Retorno registrarPasajero(String cedula, String nombre, int edad,Categoria categoria){
-        
-        if(cedula == null || nombre == null || cedula.trim().isEmpty() || nombre.trim().isEmpty())
-        {
-            
+    public Retorno registrarPasajero(String cedula, String nombre, int edad, Categoria categoria) {
+
+        if (cedula == null || nombre == null || cedula.trim().isEmpty() || nombre.trim().isEmpty()) {
+
             return new Retorno(Retorno.Resultado.ERROR_1);
-            
+
         }
-        
-        if(!cedula.matches("^\\d{1,2}\\.\\d{3}\\.\\d{3}-\\d$"))
-        {
+
+        if (!cedula.matches("^\\d{1,2}\\.\\d{3}\\.\\d{3}-\\d$")) {
             return new Retorno(Retorno.Resultado.ERROR_2);
         }
-        
-        if(edad < 0 )
-        {
+
+        if (edad < 0) {
             return new Retorno(Retorno.Resultado.ERROR_3);
         }
-        
+
         //Si ya existe un pasajero registrado con esa cédula.
-        for(int i = 0; i < pasajeros.Longitud(); i++)
-        {
+        for (int i = 0; i < pasajeros.Longitud(); i++) {
             Pasajero pasajeroExiste = pasajeros.Obtener(i);
-            if(pasajeroExiste.getCedula().equalsIgnoreCase(cedula))
-            {
+            if (pasajeroExiste.getCedula().equalsIgnoreCase(cedula)) {
                 return new Retorno(Retorno.Resultado.ERROR_4);
             }
         }
-        
+
         //si pasa todos los errores entonces creamos el nuevo pasajero con esos datos
-        Pasajero pasajero = new Pasajero(nombre,cedula,edad,categoria);
-        
+        Pasajero pasajero = new Pasajero(nombre, cedula, edad, categoria);
+
         //agregamos al nuevo pasajero recien creado a la lista de pasajeros
         //Usamos el adicionarOrdenado ya que este ordena automaticamente por el CompareTo de pasajero, osea cada pasajero que se regista
         // queda ordenado por la cedula
         pasajeros.adicionarOrdenado(pasajero);
         return new Retorno(Retorno.Resultado.OK);
-        
+
     }
-    
+
     @Override
-    public Retorno buscarPasajero(String cedula)
-    {
-         if (!cedula.matches("^\\d{1,2}\\.\\d{3}\\.\\d{3}-\\d$")) 
-         {
-             return new Retorno(Retorno.Resultado.ERROR_1);
-         }
-         
-         for(int i = 0; i < pasajeros.Longitud();i++)
-         {
-             //verificamos en la lista de pasajeros si hay un pasajero con la misma cedula que la dada por parametro
-             Pasajero pasajero = pasajeros.Obtener(i);
-             if(pasajero.getCedula().equals(cedula))
-             {
-                 //si hay un pasajero con esa cedula en la lista se devuelve en valorString con el formato dado.
-                   Retorno retorno  = new Retorno(Retorno.Resultado.OK);
-               
-                    retorno.valorString = pasajero.getCedula() + ";" +
-                    pasajero.getNombre() + ";" +
-                    pasajero.getEdad() + ";" +
-                    pasajero.getCategoria();
-                 
+    public Retorno buscarPasajero(String cedula) {
+        if (!cedula.matches("^\\d{1,2}\\.\\d{3}\\.\\d{3}-\\d$")) {
+            return new Retorno(Retorno.Resultado.ERROR_1);
+        }
+
+        for (int i = 0; i < pasajeros.Longitud(); i++) {
+            //verificamos en la lista de pasajeros si hay un pasajero con la misma cedula que la dada por parametro
+            Pasajero pasajero = pasajeros.Obtener(i);
+            if (pasajero.getCedula().equals(cedula)) {
+                //si hay un pasajero con esa cedula en la lista se devuelve en valorString con el formato dado.
+                Retorno retorno = new Retorno(Retorno.Resultado.OK);
+
+                retorno.valorString = pasajero.getCedula() + ";"
+                        + pasajero.getNombre() + ";"
+                        + pasajero.getEdad() + ";"
+                        + pasajero.getCategoria();
+
                 return retorno;
-             }
-             
-         }
-         
-         //Si no existe un pasajero registrado con esa cédula.
-         return new Retorno(Retorno.Resultado.ERROR_2);
+            }
+
+        }
+
+        //Si no existe un pasajero registrado con esa cédula.
+        return new Retorno(Retorno.Resultado.ERROR_2);
     }
-    
+
     @Override
-    public Retorno listarPasajerosAscendente()
-    {
+    public Retorno listarPasajerosAscendente() {
         Retorno retorno = new Retorno(Retorno.Resultado.OK);
 
         String resultado = "";
@@ -113,10 +103,10 @@ public class ImplementacionSistema implements ISistema {
 
             Pasajero pasajero = pasajeros.Obtener(i);
 
-            resultado += pasajero.getCedula() + ";" +
-                         pasajero.getNombre() + ";" +
-                         pasajero.getEdad() + ";" +
-                         pasajero.getCategoria();
+            resultado += pasajero.getCedula() + ";"
+                    + pasajero.getNombre() + ";"
+                    + pasajero.getEdad() + ";"
+                    + pasajero.getCategoria();
 
             if (i < pasajeros.Longitud() - 1) {
                 resultado += "|";
@@ -127,6 +117,28 @@ public class ImplementacionSistema implements ISistema {
 
         return retorno;
     }
-    
-    
+
+    @Override
+    public Retorno listarPasajerosDescendente() {
+        Retorno retorno = new Retorno(Retorno.Resultado.OK);
+        String resultado = "";
+        
+        // No se si seria solo pasajeros.Longitud() o con el -1 como está
+        for (int i = pasajeros.Longitud() - 1; i >= 0; i--) {
+            Pasajero pasajero = pasajeros.Obtener(i);
+
+            resultado += pasajero.getCedula() + ";"
+                    + pasajero.getNombre() + ";"
+                    + pasajero.getEdad() + ";"
+                    + pasajero.getCategoria();
+
+            if (i > 0) {
+                resultado += "|";
+            }
+        }
+
+        retorno.valorString = resultado;
+        return retorno;
+    }
+
 }
