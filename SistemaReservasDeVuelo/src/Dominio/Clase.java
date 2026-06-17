f628a6828951964e4cc4c47ce727fd610bf45945
Ian Britos Borgunder <IB319561@fi365.ort.edu.uy>
2026-06-16 21:39:14 -0300

    Todos los ejercicios pasaron los test, falta terminar el documento

---
+++ SistemaReservasDeVuelo/src/Dominio/Clase.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Dominio;
+
+/**
+ *
+ * @author hlbc7
+ */
+public enum Clase {
+      EJECUTIVA,
+    ECONOMICA,
+    PRIMERA
+}
